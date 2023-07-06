f7243ec406db4137f5aac562c4272471e5274928
dohaseif <dohaseif2@gmail.com>
2023-07-06 18:45:12 +0200

    crud api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,30 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//read
+Route::get('/posts',function(){
+   return Post::all();
+});
+//create
+Route::post('/posts',function (){
+
+    return Post::create([
+            'name' =>request('name'),
+            'title'=>request('title')
+        ]);
+});
+//update
+Route::put('/posts/{post}',function (Post $post){
+     $post->update([
+        'name'=>request('name'),
+        'title'=>request('title')
+    ]);
+    return response()->json($post,200);
+});
+//delete
+Route::delete('/posts/{post}',function (Post $post){
+   $success= $post->delete($post);
+   return response()->json($success,200);
+});
+
